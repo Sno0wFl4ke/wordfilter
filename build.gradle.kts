2ad564b17702bbfb97202bcccabde56762ce7c35
Sno0wFl4ke <beeadev@outlook.com>
2024-09-05 16:50:12 +0200

    feat: Added project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "me.sno0wfl4ke"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
+    implementation("com.opencsv:opencsv:5.5.2")
+    implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
---
+++ src/main/kotlin/me/sno0wfl4ke/wordfilter/WordFilter.kt
@@ -0,0 +1,95 @@
+package me.sno0wfl4ke.wordfilter
+
+import com.opencsv.CSVReader
+import me.sno0wfl4ke.wordfilter.utils.HashFilter
+import java.io.InputStreamReader
+import java.lang.IllegalArgumentException
+
+fun main() {
+    val wordFilter = WordFilter()
+    wordFilter.run()
+
+    println()
+    println()
+}
+
+class WordFilter {
+
+    private var loaded = false
+    private val hashFilter = HashFilter()
+
+    private fun loadWords(filePath: String) {
+        if (loaded) {
+            throw IllegalArgumentException("[wordfilter] Already loaded all entries!")
+        }
+
+        val inputStream = this::class.java.getResourceAsStream("/$filePath")
+            ?: throw IllegalArgumentException("Resource not found: /$filePath")
+
+        val reader = CSVReader(InputStreamReader(inputStream))
+        val lines = reader.readAll()
+
+        lines.forEach { line ->
+            //trie.insertNode(line[0])
+            hashFilter.insertAllWords(listOf(line[0]))
+        }
+
+        loaded = true
+    }
+
+    private fun searchWord(word: String): Boolean {
+        if (!loaded) {
+            throw IllegalArgumentException("[wordfilter] No entries loaded!")
+        }
+
+        return hashFilter.findWord(word)
+    }
+
+    private fun analyseString(input: String): String {
+        var censoredString = input
+        val strArray = input.lowercase().split(" ").toTypedArray()
+        var badWordCounter = 0
+        var wordCount = 0
+
+        removePunctuation(strArray.toString())
+        val runtime: HashMap<String, Long> = HashMap()
+        runtime["analyseStringBoth"] = System.currentTimeMillis()
+
+        var wordFound = false
+
+        strArray.forEach { word ->
+            wordFound = searchWord(removePunctuation(word))
+            if (wordFound) {
+                badWordCounter += 1
+                censoredString = censoredString.replace(word, censorWord(word))
+            }
+
+            wordCount += 1
+        }
+        println("Runtime analysis & censoring: ${System.currentTimeMillis() - runtime["analyseStringBoth"]!!}ms")
+        println("Harmful words: $badWordCounter/$wordCount")
+        return censoredString
+    }
+
+    private fun censorWord(word: String): String {
+        val regex = Regex("[a-zA-Z0-9]", RegexOption.IGNORE_CASE)
+        return word.replace(regex, "*")
+    }
+
+    private fun removePunctuation(sentence: String): String {
+        val regex = Regex("[=?!.,;:]", RegexOption.IGNORE_CASE)
+        return sentence.replace(regex, "")
+    }
+
+
+    fun run() {
+        loadWords("full-word-list.csv")
+        println(analyseString("Man, I was just minding my own damn business when my cat decided to knock over my coffee, spilled all over the keyboard! What a freaking mess, now my day’s all jacked up. Guess it’s just one of those hellish days, huh?"))
+
+
+
+    }
+
+
+}
+
